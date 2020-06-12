b51bde06f282aa4ec59e5e57640d4c6d76b20b83
JOAO DIVINO DA SILVA <60980853+joaodivinod@users.noreply.github.com>
2020-06-12 16:21:47 -0300

    WINP

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD:public/scripts/index.js
+<<<<<<< HEAD:public/scripts/index.js
 const buttonSearch = document.querySelector("#page-home main a")
 const modal = document.querySelector("#modal")
 const close = document.querySelector("#modal .header a")
@@ -11,5 +12,7 @@ close.addEventListener("click", () => {
     modal.classList.add("hide")
 })
 
+=======
+>>>>>>> parent of 8798824... search-results terminada:js/index.js
 =======
 >>>>>>> parent of 8798824... search-results terminada:js/index.js
